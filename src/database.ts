24564e66ce8b48ecdb4cd206a58386ed5880bd2c
Lucas Cordeiro <lcpereira@positivo.com.br>
2023-07-02 11:55:49 -0300

    feat: add sqlite

---
+++ src/database.ts
@@ -0,0 +1,9 @@
+import { knex as setupKnex } from 'knex'
+
+export const knex = setupKnex({
+  client: 'sqlite',
+  connection: {
+    filename: './temp/app.db',
+  },
+  useNullAsDefault: true,
+})
